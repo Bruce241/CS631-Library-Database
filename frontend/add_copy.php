f7ad03296bbc8567dfdaa51fb1849b5bdf69e49f
Bruce Kirschbaum <84349890+Bruce241@users.noreply.github.com>
2022-12-10 19:27:11 -0500

    Create add_copy.php

---
+++ frontend/add_copy.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+
+<form action="../backend/add_copy.php" method="post">
+  <label for="doc_id">Enter the document ID:</label><br>
+  <input type="text" id="doc_id" name="doc_id"><br>
+  <label for="copy_no">Enter the copy number:</label><br>
+  <input type="text" id="copy_no" name="copy_no"><br>
+  <label for="branch_id">Enter the branch ID:</label><br>
+  <input type="text" id="branch_id" name="branch_id"><br>
+  <input type="submit" value="Add copy"><br><br>
+  <a href="admin.php">Back</a>
+</form>
